e19dab208c56b7af8aa82e9fb1c203db7fd4b45d
AlexBangs7 <alexandrabangs@gmail.com>
2026-08-28 11:20:01 -0700

    Add argument for test file swapping with peers, make it mutually exclusive with argument for my test files

--- best_hit.py
+++ best_hit.py
@@ -4,17 +4,22 @@ import argparse
 
 def get_args():
     parser=argparse.ArgumentParser()
-    parser.add_argument("-s1", help="species1", required=True, choices=['Hsa','Dre','Eel','Pka'])
-    parser.add_argument("-s2", help="species2", required=True, choices=['Hsa','Dre','Eel','Pka'])
-    parser.add_argument("-t", help="running on test files", required=False)
+    parser.add_argument("-s1","--species1", help="First species", required=True, choices=['Hsa','Dre','Eel','Pka'])
+    parser.add_argument("-s2","--species2", help="Second species", required=True, choices=['Hsa','Dre','Eel','Pka'])
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--test", help="running on test files", required=False)
+    group.add_argument("--swapPeer", help="running on peer test files. Provide initials of peer, and make sure file names match requirements", required=False)
     return parser.parse_args()
 args = get_args()
 
 species1 = args.s1
 species2 = args.s2
+swapPeer = args.swapPeer
 
 def best_hit(species1=str, species2=str):
-    if args.t:
+    if args.swapPeer:
+            BLASTp_file = open(f"/projects/bgmp/abangs/bioinfo/Bi623/PS/AlexBangs7-Bi623-PS1/test-files/peer-swap/{swapPeer}_{species1}_{species2}.txt", "r")
+    elif args.test:
         BLASTp_file = open(f"/projects/bgmp/abangs/bioinfo/Bi623/PS/AlexBangs7-Bi623-PS1/test-files/{species1}_{species2}_test.txt", "r")
     else:
         BLASTp_file = open(f"/projects/bgmp/shared/Bi623/PS1/blasthits/{species1}_query_{species2}_db.txt", "r")
@@ -48,8 +53,11 @@ def best_hit(species1=str, species2=str):
 
     # dictionary for identifying all protein IDs and their corresponding gene IDs
     gene_dict = {} # key = protein stable ID, value = [gene stable ID, gene name]
-    with open(f"/projects/bgmp/shared/Bi623/PS1/biomart/{species1}_biomart_v116.txt", "r") as tbl:
-        for index, line in enumerate(tbl):
+    if args.swapPeer:
+        gene_table_file = open(f"/projects/bgmp/abangs/bioinfo/Bi623/PS/AlexBangs7-Bi623-PS1/test-files/peer-swap/{species1}_gene_table.txt", "r")
+    else:
+        gene_table_file = open(f"/projects/bgmp/shared/Bi623/PS1/biomart/{species1}_biomart_v116.txt", "r")
+        for index, line in enumerate(gene_table_file):
             if index == 0:
                 continue
             gene_info = line.strip('\n').split('\t')
@@ -64,11 +72,13 @@ def best_hit(species1=str, species2=str):
 species1_dict, species1_genes = best_hit(species1, species2)
 species2_dict, species2_genes = best_hit(species2, species1)
 
-# Take results of best_hit and print reciprocal best hits to output tsv file
-if args.t:
+# 3. Take results of best_hit and print reciprocal best hits to output tsv file
+if args.swapPeer:
+    output_file = open(f"/projects/bgmp/abangs/bioinfo/Bi623/PS/AlexBangs7-Bi623-PS1/test-files/peer-swap/{swapPeer}_{species1}_{species2}_output_RBH.tsv", "w")
+elif args.test:
     output_file = open(f"/projects/bgmp/abangs/bioinfo/Bi623/PS/AlexBangs7-Bi623-PS1/test-files/{species1}_{species2}_output_RBH.tsv", "w")
 else:
-    output_file = open(f'/projects/bgmp/abangs/bioinfo/Bi623/PS/AlexBangs7-Bi623-PS1/output-files/{species1}_{species2}_RBH.tsv',"w")
+    output_file = open(f'/projects/bgmp/abangs/bioinfo/Bi623/PS/AlexBangs7-Bi623-PS1/output-files/{species1}_{species2}_RBH_rv.tsv',"w")
 
 output_file.write(f'{species1} Gene ID\t{species1} Protein ID\t{species1} Gene Name\t{species2} Gene ID\t{species2} Protein ID\t{species2} Gene Name\n')
 
@@ -79,5 +89,5 @@ for species1_hit, species2_hit in species1_dict.items():
         gene2_id, gene2_name = species2_genes[species2_hit][0:2]
         #gene2_name = species2_genes[species2_hit][1]
 
-        output_file.write(f'{gene1_id}\t{species1_hit}\t{gene1_name}\t{gene2_id}\t{species2_hit}\t{gene2_name}\n')
+        output_file.write(f'{species1_hit}\t{gene1_id}\t{gene1_name}\t{species2_hit}\t{gene2_id}\t{gene2_name}\n')
 output_file.close()
