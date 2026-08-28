d02d8ad5893364d7b1a2e1d4621925a85cd9a530
AlexBangs7 <alexandrabangs@gmail.com>
2026-08-27 19:46:48 -0700

    change output file formating to include species code in header, rather than species 1 and species 2

--- best_hit.py
+++ best_hit.py
@@ -6,6 +6,7 @@ def get_args():
     parser=argparse.ArgumentParser()
     parser.add_argument("-s1", help="species1", required=True, choices=['Hsa','Dre','Eel','Pka'])
     parser.add_argument("-s2", help="species2", required=True, choices=['Hsa','Dre','Eel','Pka'])
+    parser.add_argument("--test", help="running on test files", required=False, choices=[True,False])
 
     return parser.parse_args()
 args = get_args()
@@ -13,26 +14,35 @@ args = get_args()
 species1 = args.s1
 species2 = args.s2
 
-
 def best_hit(species1=str, species2=str):
+    if args.test == True:
+        BLASTp_file = open(f"/projects/bgmp/abangs/bioinfo/Bi623/PS/AlexBangs7-Bi623-PS1/test-files/{species1}_{species2}_test.txt", "r")
+    else:
+        BLASTp_file = open(f"/projects/bgmp/shared/Bi623/PS1/blasthits/{species1}_query_{species2}_db.txt", "r")
 
     # Iterate over sorted BLASTp results for species and make dictionary of best hits
-    with open(f"/projects/bgmp/shared/Bi623/PS1/blasthits/{species1}_query_{species2}_db.txt", "r") as file:
-        eval_dict = {} # key = query sequence ID, value = [subject sequence ID, e-value, duplicate-flag]
-        for line in file:
-            line = line.strip("\n").split()
-            queryID = line[0]
-            subjectID = line[1]
-            evalue = float(line[10])
-            if queryID in eval_dict:
-                if evalue == eval_dict[queryID][1] and subjectID != eval_dict[queryID][0]:
-                    eval_dict[queryID][2] = True
-                    #mark identical flag as TRUE # if subject ID is the same as dictionary's subjectID, then should not mark as duplicate
+    eval_dict = {} # key = query sequence ID, value = [subject sequence ID, e-value, duplicate-flag]
+    for line in BLASTp_file:
+        if line.startswith("#") or line == "":
+            continue # skip header lines in test files
+        line = line.strip("\n").split()
+        queryID = line[0]
+        subjectID = line[1]
+        evalue = float(line[10])
+        if queryID in eval_dict:
+            if evalue == eval_dict[queryID][1] and subjectID != eval_dict[queryID][0]:
+                eval_dict[queryID][2] = True 
             else:
-                eval_dict[queryID] = [subjectID, evalue, False] #add to dictionary {query: [subjectID, evalue, FALSE duplicate flag]}
+                continue
+                # 1. if e-vals are different and subjectIDs are different, you already have best hit for different subjectID; 
+                # 2. if e-vals are different and subjectIDs are same, you already have best hit for same subjectID; 
+                # 3. if e-vals are same and subjectIDs are same, you already have hit for subjectID (and keep it)
+        else: # add queryID and best hit to eval_dict
+            eval_dict[queryID] = [subjectID, evalue, False]
+    BLASTp_file.close()
 
     # Condense eval_dict by removing best hits that had identical flags for different subject sequence IDs
-    best_hit_dict = {} # key = query sequence ID, value = subject sequence ID
+    best_hit_dict = {} # key = query sequence ID, value = subject sequence ID (no need for e-value anymore)
     for key, value in eval_dict.items():
         if value[2] == False:
             best_hit_dict[key] = value[0]
@@ -55,16 +65,20 @@ def best_hit(species1=str, species2=str):
 species1_dict, species1_genes = best_hit(species1, species2)
 species2_dict, species2_genes = best_hit(species2, species1)
 
-# Take results of best_hit and print reciprocal best hits to tsv file
-with open(f'{species1}_{species2}_RBH.tsv',"w") as output:
-    output.write('Species 1 Gene ID\tSpecies 1 Protein ID\tSpecies 1 Gene Name\tSpecies 2 Gene ID\tSpecies 2 Protein ID\tSpecies 2 Gene Name\n')
-    for species1_hit, species2_hit in species1_dict.items():
-        # print(species1_hit, species2_hit)
-        if species2_hit in species2_dict and species2_dict[species2_hit] == species1_hit:
+# Take results of best_hit and print reciprocal best hits to output tsv file
+if args.test == True:
+    output_file = open(f"/projects/bgmp/abangs/bioinfo/Bi623/PS/AlexBangs7-Bi623-PS1/test-files/{species1}_{species2}_test_output.txt", )
+else:
+    output_file = open(f'/projects/bgmp/abangs/bioinfo/Bi623/PS/AlexBangs7-Bi623-PS1/output-files/{species1}_{species2}_RBH.tsv',"w")
 
-            gene1_id = species1_genes[species1_hit][0]
-            gene1_name = species1_genes[species1_hit][1]
-            gene2_id = species2_genes[species2_hit][0]
-            gene2_name = species2_genes[species2_hit][1]
+output_file.write(f'{species1} Gene ID\t{species1} Protein ID\t{species1} Gene Name\t{species2} Gene ID\t{species2} Protein ID\t{species2} Gene Name\n')
 
-            output.write(f'./Output-files/{gene1_id}\t{species1_hit}\t{gene1_name}\t{gene2_id}\t{species2_hit}\t{gene2_name}\n')
+for species1_hit, species2_hit in species1_dict.items():
+    if species2_hit in species2_dict and species2_dict[species2_hit] == species1_hit:
+        gene1_id, gene1_name = species1_genes[species1_hit][0:2]
+        #gene1_name = species1_genes[species1_hit][1]
+        gene2_id, gene2_name = species2_genes[species2_hit][0:2]
+        #gene2_name = species2_genes[species2_hit][1]
+
+        output_file.write(f'./Output-files/{gene1_id}\t{species1_hit}\t{gene1_name}\t{gene2_id}\t{species2_hit}\t{gene2_name}\n')
+output_file.close()
